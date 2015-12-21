a280cd07a4708a96c7913d2006adc70b7c866a39
David Omid <davidomid@hotmail.com>
2015-12-21 03:12:52 +0000

    First commit

---
+++ ContentHiderModule/ContentHiderModule/ContentHiderModule.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Web;
+
+namespace ContentHiderModule
+{
+    public class ContentHiderModule : IHttpModule
+    {
+        private EventHandler onBeginRequest;
+
+        private Func<HttpContext, bool> isAllowedFunc;
+
+        public ContentHiderModule(Func<HttpContext, bool> isAllowedFunc)
+        {
+            this.isAllowedFunc = isAllowedFunc;
+            onBeginRequest = new EventHandler(this.HandleBeginRequest);
+        }
+
+        private void HandleBeginRequest(object sender, EventArgs evargs)
+        {
+            HttpApplication app = sender as HttpApplication;
+            if (!isAllowedFunc.Invoke(app.Context))
+            {
+                app.Context.Response.StatusCode = 404;
+                app.Context.Response.SuppressContent = true;
+                app.Context.Response.End();
+            }
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += onBeginRequest;
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
